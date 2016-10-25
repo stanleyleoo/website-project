5ab39127a65ab1f13a8ca7c085ea6bcf92e6139e
stanleyleoo <stanz_leo@yahoo.com>
2016-10-25 16:23:47 +0700

    login updated

--- login.php
+++ login.php
@@ -4,38 +4,48 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Login</title>
 </head>
 <body>
 <?php
 require_once "db.php";
 
-if(!isset($_POST['username']) && !isset($_POST['password']))
+if(!isset($_POST['username']) && !isset($_POST['password'])){
 	echo "username dan password tidak cocok";
+}
+else {
+	$username = $_POST['username'];
+	$password = $_POST['password'];
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+	$conn = konek_db();;
+	$query = $conn->prepare("select password,category from user where username=?");
+	$query->bind_param("s",$username);
+	$result = $query->execute();
+	$query->bind_result($passdb, $category);
+	$query->fetch();
 
-$conn = konek_db();
-$query = $conn->prepare("select * from user");
-$result = $query->execute();
+	if(!$result)
+		die("Gagal koneksi ke database");
 
-if(!$result)
-	die("Gagal koneksi ke database");
-$rows = $query->get_result();
+		if($password != null || $password != ''){
+			if($passdb = $password){
+				$_SESSION['username'] = $username;
+				switch ($category) {
+					case 1:
+						header("Location:admin.php");
+						break;
+					
+					
+				}
+			}
+		
 
-$row = $rows->fetch_array()
-if($row){
-	if($username == $row['username'] && $password == $row['password'] ){
-		$_SESSION['username'] = $username;
-		$_SESSION['category'] = $row->category;
-		switch($row->category){
-			case 1: $loc = "location:admin.php";
-			break;
-			case 2: $loc = "location:input-news1.php";
-			break;
-		}
-		header($loc);
+		// switch($row->category){
+		// 	case 1: $loc = "location:admin.php";
+		// 	break;
+		// 	case 2: $loc = "location:input-news1.php";
+		// 	break;
+		// }
 	}else
 		die("password atau username salah");
 }
